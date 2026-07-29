8fa445328e0f44a7b76a3383b203bff8f787f6de
AstralFC <trouvaillebins@gmail.com>
2026-07-28 20:13:18 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    GUILD_ID: "1521671990505635965",
+
+    OWNER_ID: "1505021865985572940",
+
+    CO_OWNER_ID: "1429837765281058876",
+
+    MAX_PLAYERS: 15,
+
+    LEAGUE_NAME: "VVLL",
+
+    POINTS: {
+        WIN: 3,
+        DRAW: 1,
+        LOSS: 0
+    }
+
+};
