f70e34e60dbefead0d90f8e0f8819589b9615da5
AstralFC <trouvaillebins@gmail.com>
2026-07-28 20:38:23 -0700

    Update league.js

--- league.js
+++ league.js
@@ -4,214 +4,282 @@
 // =====================================
 
 const database = require("./database");
+const utils = require("./utils");
 
 
-// Create a new league
-function createLeague(teamNames) {
 
-    const teams = randomizeTeams(teamNames);
 
 
-    database.db.settings = {
-        league: "VVLL",
-        active: true,
-        week: 1
-    };
+// Create league
 
+function createLeague(teamNames){
 
-    database.db.teams = teams.map((team, index) => ({
-        id: index + 1,
-        name: team,
-        players: [],
-        points: 0,
-        wins: 0,
-        draws: 0,
-        losses: 0,
-        goalsFor: 0,
-        goalsAgainst: 0
-    }));
 
 
-    database.db.matches = generateSchedule(teams);
+    database.db.settings.active = true;
 
 
-    database.save();
+    database.db.settings.league = "VVLL";
 
-
-    return database.db;
-
-}
-
-
-
-// Randomize teams
-function randomizeTeams(teams) {
-
-    return [...teams].sort(
-        () => Math.random() - 0.5
-    );
-
-}
-
-
-
-// Generate matches
-function generateSchedule(teams) {
-
-    let matches = [];
-
-    let id = 1;
-
-
-    for(let i = 0; i < teams.length; i++) {
-
-        for(let j = i + 1; j < teams.length; j++) {
-
-
-            matches.push({
-
-                id: id++,
-
-                week: 1,
-
-                home: teams[i],
-
-                away: teams[j],
-
-                homeScore: null,
-
-                awayScore: null,
-
-                timestamp: null,
-
-                played: false
-
-            });
-
-
-        }
-
-    }
-
-
-    return matches;
-
-}
-
-
-
-// Add match time
-function setMatchTime(matchId, timestamp) {
-
-    let match = database.db.matches.find(
-        m => m.id === matchId
-    );
-
-
-    if(!match) return false;
-
-
-    match.timestamp = timestamp;
-
-
-    database.save();
-
-
-    return true;
-
-}
-
-
-
-// Finish a match
-function finishMatch(matchId, homeScore, awayScore) {
-
-    let match = database.db.matches.find(
-        m => m.id === matchId
-    );
-
-
-    if(!match) return false;
-
-
-    match.homeScore = homeScore;
-
-    match.awayScore = awayScore;
-
-    match.played = true;
-
-
-    database.save();
-
-
-    return match;
-
-}
-
-
-
-// Get league
-function getLeague() {
-
-    return database.db;
-
-}
-
-
-
-// Reset league
-function resetLeague() {
-
-
-    database.db.settings = {
-
-        league:"VVLL",
-
-        active:false
-
-    };
-
-
-    database.db.teams = [];
 
     database.db.matches = [];
 
 
+
+
+
+    let shuffled = utils.shuffle(teamNames);
+
+
+
+
+
+    database.db.teams = shuffled.map(
+
+        (name,index)=>({
+
+
+            id: utils.randomId(),
+
+
+            name:name,
+
+
+            manager:null,
+
+
+            players:[],
+
+
+            points:0,
+
+
+            wins:0,
+
+
+            draws:0,
+
+
+            losses:0,
+
+
+            goalsFor:0,
+
+
+            goalsAgainst:0,
+
+
+            played:0,
+
+
+            seed:index+1
+
+
+        })
+
+
+    );
+
+
+
+
+
+    generateSchedule();
+
+
+
+
     database.save();
+
+
+
+
+    return database.db.teams;
 
 }
 
 
 
-// Next week
-function nextWeek(){
 
-    database.db.settings.week++;
+
+
+
+
+// Create match schedule
+
+function generateSchedule(){
+
+
+
+    let teams = database.db.teams;
+
+
+
+    let week = 1;
+
+
+
+
+    for(let i = 0; i < teams.length; i++){
+
+
+
+        for(let j = i + 1; j < teams.length; j++){
+
+
+
+            database.db.matches.push({
+
+
+                id: utils.randomId(),
+
+
+                week:week,
+
+
+                home:teams[i].name,
+
+
+                away:teams[j].name,
+
+
+                homeScore:null,
+
+
+                awayScore:null,
+
+
+                completed:false,
+
+
+                time:null
+
+
+            });
+
+
+
+            week++;
+
+            if(week > 99){
+
+                week = 1;
+
+            }
+
+
+        }
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+// Set game time
+
+function setGameTime(gameId,time){
+
+
+
+    let game = database.db.matches.find(
+
+        g=>g.id === gameId
+
+    );
+
+
+
+    if(!game){
+
+        return false;
+
+    }
+
+
+
+
+    game.time = time;
+
 
 
     database.save();
 
+
+
+    return true;
+
+
 }
+
+
+
+
+
+
+
+
+// Get upcoming games
+
+function getGames(){
+
+
+
+    return database.db.matches;
+
+}
+
+
+
+
+
+
+
+
+// Reset league
+
+function resetLeague(){
+
+
+
+    database.reset();
+
+
+    return true;
+
+
+}
+
+
+
 
 
 
 
 module.exports = {
 
+
     createLeague,
 
-    randomizeTeams,
 
     generateSchedule,
 
-    setMatchTime,
 
-    finishMatch,
+    setGameTime,
 
-    getLeague,
 
-    resetLeague,
+    getGames,
 
-    nextWeek
+
+    resetLeague
+
 
 };
