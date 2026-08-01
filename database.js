f1d3e781f5b80d2ddb1d35bb5b44ac2203aef168
AstralFC <trouvaillebins@gmail.com>
2026-07-31 20:08:05 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,54 +1,5 @@
-// =======================================
-// VVLL BOT DATABASE
-// =======================================
-
-const fs = require("fs");
-
-const FILE = "./database.json";
-
-let data = {
-    teams: [],
-    players: [],
-    games: []
-};
-
-function load() {
-    if (fs.existsSync(FILE)) {
-        try {
-            data = JSON.parse(fs.readFileSync(FILE, "utf8"));
-            console.log("✅ Database loaded");
-        } catch (err) {
-            console.log("⚠️ Database corrupted, creating a new one...");
-            save();
-        }
-    } else {
-        save();
-    }
+{
+    "teams": {},
+    "players": {},
+    "games": {}
 }
-
-function save() {
-    fs.writeFileSync(
-        FILE,
-        JSON.stringify(data, null, 4)
-    );
-}
-
-function reset() {
-    data = {
-        teams: [],
-        players: [],
-        games: []
-    };
-
-    save();
-}
-
-module.exports = {
-    get data() {
-        return data;
-    },
-
-    load,
-    save,
-    reset
-};
